1bff5d718d5bd771ec76f6342aa6a3af80576e8a
Devon <devonev92@gmail.com>
2019-09-17 10:54:28 -0700

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: path.resolve(__dirname, './client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/build',
+    proxy: {
+      '/api': 'http://localhost:3000'
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
