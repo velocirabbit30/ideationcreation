b130b88a468637d37a52c7a1c275b03c6676c4cf
Devon <devonev92@gmail.com>
2019-09-17 18:33:28 -0700

    built db.js and tables

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+require('dotenv').config();
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
