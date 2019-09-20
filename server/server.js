57f20c0da66c479e31a5ad5994fbf243601f564b
Devon <devonev92@gmail.com>
2019-09-19 18:34:31 -0700

    added a graveyard.js file

--- server/server.js
+++ server/server.js
@@ -7,10 +7,16 @@ const authRouter = require('./routes/auth.js');
 // const db = require('../server/db/db.js')
 const projectsRoutes = require('./routes/projects');
 const techRoutes = require('./routes/tech');
+// const graveyardRoutes = require('./routes/graveyard');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+<<<<<<< Updated upstream
+=======
+
+app.use(bodyParser.json());
+>>>>>>> Stashed changes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
@@ -31,11 +37,11 @@ res.status(400).json({
 
 
 // routes
-app.use('/api/tech', techRoutes)
+app.use('/api/tech', techRoutes);
 app.use('/api/projects', projectsRoutes);
-
+// app.use('/api/graveyard', graveyardRoutes);
 // global error handler
-app.use((req, res, next, err) => {
+app.use(function (err, req, res, next) {
 res.status(400).json({
   errorMessage: 'no route exists that handles your request',
   error: err
